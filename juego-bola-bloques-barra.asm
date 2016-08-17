607862de3a3fe75a67e22082221668232936c00d
AMP <casco03@gmail.com>
2016-08-16 20:19:28 -0600

    juego-bola-bloques-barra.asm

--- juego-bola-bloques-barra.asm
+++ juego-bola-bloques-barra.asm
@@ -1474,12 +1474,56 @@ _start:
 .perdio:
         print msm_pierde1,tamano_msm_pierde1            	;despliega la pantalla de perdida y espera la tecla X para salir
         ;call Pausa
-       	mov rax,80000000H
+       	;mov rax,80000000H
+	;cpuid
+	;mov r8,80000004H
+	;cmp rax,r8
+	;;jb _default
+	; obtienen los datos a imprimir 
+
+	;mov rax,80000002H
+	;cpuid
+	;mov [var],rax
+	;mov [var + 0x4],rbx; informacion de procesador
+	;mov [var + 0x8],rcx; identidicacion de la familia
+	;mov [var + 0xc],rdx; ; tipo de procesador
+
+	;mov rax,80000003H
+	;cpuid
+	;mov [var + 0x10],rax
+	;mov [var + 0x14],rbx; informacion de procesador
+	;mov [var + 0x18],rcx ;identidicacion de la familia
+	;mov [var + 0x1c],rdx; tipo de procesador
+
+	;mov rax,80000004H
+	;cpuid
+	;mov [var + 0x20],rax
+	;mov [var + 0x24],rbx; informacion de procesador
+	;mov [var + 0x28],rcx; identidicacion de la familia
+	;mov [var + 0x2c],rdx; tipo de procesador
+
+	; se imprime los datos del cpu
+	;irs 2610,2615,2608,2608
+	;print var,50
+	;print mns2,tamano_mns2
+	jmp .if
+.if:
+        mov word [let],1                                ;Limpia variable let
+        in_teclado let,1                                ;Copia, de ser posible, la tecla que se este presionando en [let]
+        cmp word [let],110                              ;Compara si la letra presionada es "N"
+        je  .creditos                                        ;De no ser "N" el juego sigue en pausa y revisa por la siguiente tecla
+	cmp word [let],115                              ;De no ser "S" el juego sigue en pausa
+        je  .restart
+        jmp .if
+
+.creditos:
+        print msm_creditos,tamano_msm_creditos
+        mov rax,80000000H
 	cpuid
 	mov r8,80000004H
 	cmp rax,r8
 	;jb _default
-	; obtienen los datos a imprimir 
+	;obtienen los datos a imprimir 
 
 	mov rax,80000002H
 	cpuid
@@ -1506,20 +1550,8 @@ _start:
 	irs 2610,2615,2608,2608
 	print var,50
 	print mns2,tamano_mns2
-	jmp .if
-.if:
-        mov word [let],1                                ;Limpia variable let
-        in_teclado let,1                                ;Copia, de ser posible, la tecla que se este presionando en [let]
-        cmp word [let],110                              ;Compara si la letra presionada es "N"
-        je  .fin                                        ;De no ser "N" el juego sigue en pausa y revisa por la siguiente tecla
-	cmp word [let],115                              ;De no ser "S" el juego sigue en pausa
-        je  .restart
-        jmp .if
-
-;.creditos:
-;        print msm_creditos,tamano_msm_creditos
-;        call .pausa
-;        jmp .fin
+        call Pausa
+        jmp .fin
 
 .fin:
 	call canonical_on				;Se vuelve a encender el modo canonico
@@ -1528,7 +1560,3 @@ _start:
 	mov rax,60
 	mov rdi,0
 	syscall
-
-
-
-
